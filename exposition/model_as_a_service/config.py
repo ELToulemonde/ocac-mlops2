0bbca749a6807d218a27227e74d81aa95d99e6a7
Baptiste O'Jeanson <bojeanson@octo.com>
2021-04-07 19:25:43 +0200

    add TP7

--- exposition/model_as_a_service/config.py
+++ exposition/model_as_a_service/config.py
@@ -1,6 +1,9 @@
 import os
 
-PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+INFERENCE_HOST = os.environ.get('INFERENCE_HOST', 'localhost')
+
+PROJECT_FOLDER = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, 'data')
 PREDICTIONS_FOLDER = os.path.join(DATA_FOLDER, 'predictions')
@@ -11,5 +14,7 @@ GENERATED_DATA_PATH = os.path.join(DATA_FOLDER,
 
 FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
-MODEL_REGISTRY_FOLDER = os.path.join(PROJECT_FOLDER, 'models')
-MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER, '20200616-140746.joblib')  # To change when needed
+MODEL_REGISTRY_FOLDER = '.'
+MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER,
+                          '20200616-140746.joblib')  # To change when needed
+print("MODEL_PATH:", MODEL_PATH)
