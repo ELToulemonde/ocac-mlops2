2b738bb72a67e7f5a0ae314be2e41a360588f245
Baptiste O'Jeanson <bojeanson@octo.com>
2021-04-07 19:25:43 +0200

    add TP7

--- exposition/exposing_predictions/config.py
+++ exposition/exposing_predictions/config.py
@@ -1,14 +1,15 @@
 import os
 
-PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_FOLDER = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print("PROJECT_FOLDER:", PROJECT_FOLDER)
 
 DATA_FOLDER = os.path.join(PROJECT_FOLDER, 'data')
-GENERATED_DATA_FOLDER = os.path.join(DATA_FOLDER, "generated_data")
-PREDICTIONS_FOLDER = os.path.join(DATA_FOLDER, 'predictions')
+PREDICTIONS_FOLDER = '.'
+print("PREDICTIONS_FOLDER:", PREDICTIONS_FOLDER)
 
 TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
-# TODO change, should point to generator
-GENERATED_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
+GENERATED_DATA_PATH = os.path.join(DATA_FOLDER,
+                                   'la-haute-borne-data-2017-2020.csv')  # TODO change, should point to generator
 
 FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
 
